fdac01984630bcc766c38370e585bc78bf54e5e9
Chostakovitch <quentinduchemin@gmail.com>
2016-05-25 14:34:31 +0200

    unsigned_int -> size_t pour les fonctions type string::find

--- UTComputer/main.cpp
+++ UTComputer/main.cpp
@@ -104,7 +104,7 @@ int main(int argc, char *argv[])
     /*try {
         Manager::getInstance().handleOperandLine("[1 [4 +]");
     try {
-        Manager::getInstance().handleOperandLine("\"SIN(8) + 4\"");
+        Manager::getInstance().handleOperandLine("\"SIN(8) + 4\" 4");
         Manager::getInstance().handleOperandLine("4           3   +");
         Manager::getInstance().handleOperandLine("*");
         for(const auto& e : Manager::getInstance().getPile()) std::cout << e->toString() << " ";
